f6c3b086dd9e338c4470e57d36bb2fec72e2c6bf
joooshua-chen <130087751+joooshua-chen@users.noreply.github.com>
2023-05-28 23:43:56 -0700

    kinda complete

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # Lab 8 - Starter
+
+# Name: Joshua Chen
+
+# Check Your Understanding Question 1
+I would fit my automated tests within a Github action that runs whenever code is pushed. This is because for each iteration of the the Recipe website, you would want to test the pushed code to ensure that it maintains the same functionality along with the added developments. Manually running them locally does not ensure that it will work on other devices and running them all after development will cause difficulties in figuring out bugs.
+
+# Check Your Understanding Question 2
+No, you would not want to use an end to end test to check if a function is returning the correct output. Since E2E testing is emulating user actions from start to finish, it is checking the execution of code as a whole rather than a singular component like a function. A more suitable form of testing would be a unit test to check if a function is returning the correct output.
+
+# Check Your Understanding Question 3
+No, unit testing is for small scale debugging. the messaging feature for a messaging app is too broad for unit testing since its the main feature of the entire application
+
+# Check Your Understanding Question 4
+Yes, Since the max message length is a small part of a larger moving feature like messaging, I would use a unit test to test the max message length since it is something that should be isolated and will not affect other unit tests.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,95 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+test('bad format number', () => {
+    expect(isPhoneNumber(321333343434)).toBe(false);
+  });
+
+test('only letters', () => {
+    expect(isPhoneNumber(abc)).toBe(false);
+  });
+
+test('police', () => {
+    expect(isPhoneNumber(911)).toBe(true);
+  });
+
+  test('correct format', () => {
+    expect(isPhoneNumber(6192235467)).toBe(true);
+  });
+
+  test('bad format number ', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+  test('bad format number ', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+  test('bad format number ', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+  test('bad format number ', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+  test('bad format number ', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(false);
+  });
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
+
+  test('adds 1 + 2 to equal 3', () => {
+    expect(sum(1,2)).toBe(true);
+  });
